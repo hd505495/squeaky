d9c9750c20f419b070389501718c3846556258ca
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2025-02-26 19:06:23 +0000

    wip

--- src/Rules/Clean.php
+++ src/Rules/Clean.php
@@ -22,7 +22,7 @@ class Clean implements ValidationRule
             $tolerated = Config::get('profanify-tolerated');
 
             if (Str::contains(Str::lower(Str::remove($tolerated, $value)), $profanities)) {
-                $fail(__('messages.unclean'));
+                $fail('messages.unclean');
             }
         }
     }
