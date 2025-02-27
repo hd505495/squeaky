3a85f0d271392142ba422b525af18afbe5da7543
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2025-02-27 00:56:33 +0000

    rename

--- src/Rules/Clean.php
+++ src/Rules/Clean.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace JonPurvis\Clean\Rules;
+namespace JonPurvis\Squeaky\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace JonPurvis\Clean\Tests;
+namespace JonPurvis\Squeaky\Tests;
 
-use JonPurvis\Clean\CleanServiceProvider;
+use JonPurvis\Squeaky\SqueakyServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -15,7 +15,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app): array
     {
         return [
-            CleanServiceProvider::class,
+            SqueakyServiceProvider::class,
         ];
     }
 
